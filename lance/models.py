cfc054fe537180465a0e6bb63d3e712989388dfc
PraiseTheNewSun <142599118+PraiseTheNewSun@users.noreply.github.com>
2024-07-31 06:55:30 -0700

    Add files via upload

---
+++ lance/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Register(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    stat_list = (
+        ('talent', 'Talent'),
+        ('client', 'Client')
+    )
+    status = models.CharField(max_length=30, choices=stat_list, default='Who are you ?')
+
+    def __str__(self):
+        return self.user.username
+
+class PostJob(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField(max_length=1000)
+    budget = models.IntegerField()
+    tag_list = (
+        ('html', 'HTML'),
+        ('css', 'CSS'),
+        ('python', 'Python')
+    )
+    tags = models.CharField(max_length=50, choices=tag_list, default='Select Tags')
+    time_posted = models.DateTimeField(auto_now_add=True, null=True)
+    author = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.title
+
+class Application(models.Model):
+    job_title = models.CharField(max_length=1000)
+    job_author = models.CharField(max_length=300, null=True)
+    bid = models.IntegerField(null=True)
+    cover_letter = models.TextField(max_length=1000)
+    applicant_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.applicant_name
---
+++ lance/views.py
@@ -0,0 +1,174 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from .models import PostJob, Application, Register
+
+# Create your views here.
+##### LANDING PAGE #####
+def Home(request):
+    return render(request, 'lance/home.html')
+
+##### SIGN UP #####
+def SignUp(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = make_password(request.POST.get('password'))
+
+        user = User.objects.create(username=username, password=password, first_name=first_name, last_name=last_name, email=email)
+        user.save()
+
+    page = 'signup'
+    context = {
+        'page': page,
+    }
+    return render(request, 'lance/signup_login.html', context)
+
+##### LOG IN #####
+def Login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('client-dashboard')
+        else:
+            return redirect('login')
+
+    page = 'login'
+    context = {
+        'page': page,
+    }
+    return render(request, 'lance/signup_login.html', context)
+
+##### LOG OUT #####
+def LogOut(request):
+    if request.method == 'POST':
+        user = User.objects.get(username=request.user)
+        logout(user)
+        return redirect('home')
+    page = 'logout'
+    context = {
+        'page': page,
+    }
+    return render(request, 'lance/signup_login.html', context)
+
+##### TALENT #####
+def TalentDashboard(request):
+    page = 'talent_page'
+    jobs = PostJob.objects.all()
+    context = {
+        'page': page,
+        'jobs': jobs
+    }
+    return render(request, 'lance/dashboard.html', context)
+
+def TalentJobs(request):
+    if request.method == 'POST':
+        job_info = request.POST['job_info']
+        bid = request.POST['bid']
+        cover_letter = request.POST['cover_letter']
+        applicant_name = request.POST['applicant_name']
+        author = request.POST['author']
+
+        applications = Application.objects.create(job_title=job_info, bid=bid, cover_letter=cover_letter, applicant_name=applicant_name, job_author=author)
+        
+
+    page = 'jobs'
+    jobs = PostJob.objects.all()
+    context = {
+        'page': page,
+        'jobs': jobs
+    }
+    return render(request, 'lance/talent.html', context)
+
+def TalentApplications(request):
+    application = Application.objects.filter(applicant_name=request.user)
+
+    jobs = PostJob.objects.all()
+    page = 'applications'
+    context = {
+        'jobs': jobs,
+        'page': page,
+        'applications': application,
+    }
+    return render(request, 'lance/talent.html', context)
+
+def TalentProfile(request):
+    page = 'profile'
+    context = {
+        'page': page,
+    }
+    return render(request, 'lance/talent.html', context)
+
+def TalentHires(request):
+    page = 'hires'
+    context = {
+        'page': page,
+    }
+    return render(request, 'lance/talent.html', context)
+
+##### CLIENT #####
+def ClientDashboard(request):
+    page = 'client_page'
+    context = {
+        'page': page
+    }
+    return render(request, 'lance/dashboard.html', context)
+
+def ClientProfile(request):
+    try:
+        user_profile = User.objects.get(username=request.user)
+        profile_data = Register.objects.get(user=request.user)
+    except:
+        pass
+    page = 'profile'
+    context = {
+        'page': page,
+        'user': user_profile,
+        'profile_data': profile_data
+    }
+    return render(request, 'lance/client.html', context)
+
+def ClientProfileUpdate(request):
+    if request.method == "PUT":
+        username = request.PUT['username']
+        firstname = request.PUT['firstname']
+        lastname = request.PUT['lastname']
+        email = request.PUT['email']
+        img = request.PUT['img']
+        #print(username, fistname, lastname, email, img)
+    page = 'client_update'
+    context = {
+        'page': page,
+    }
+    return render(request, 'lance/talent.html', context)
+
+def ClientApplications(request):
+    applications = Application.objects.filter(job_author=request.user)
+
+    page = 'applications'
+    context = {
+        'page': page,
+        'applications': applications,
+    }
+    return render(request, 'lance/client.html', context)
+
+def ClientJobs(request):
+    page = 'jobs'
+    context = {
+        'page': page,
+    }
+    return render(request, 'lance/client.html', context)
+
+def ClientHires(request):
+    page = 'hires'
+    context = {
+        'page': page,
+    }
+    return render(request, 'lance/client.html', context)
