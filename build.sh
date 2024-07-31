45f000a45a291cdd525727cec555da44205d79bf
PraiseTheNewSun <142599118+PraiseTheNewSun@users.noreply.github.com>
2024-07-31 07:45:55 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
-pip3 install gunicorn
-pip3 install Django==5.0.3 pillow==10.2.0 pytz==2024.1 requests==2.31.0 sqlparse==0.4.4 tzdata==2024.1 urllib3==2.2.1 whitenoise==6.6.0
+pip install gunicorn
+pip install Django==5.0.3 
+pip install pillow==10.2.0 
+pip install pytz==2024.1 
+pip install requests==2.31.0 
+pip install sqlparse==0.4.4 
+pip install tzdata==2024.1 
+pip install urllib3==2.2.1 
+pip install whitenoise==6.6.0
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic
-echo
-echo yes
+python manage.py collectstatic --no-input
