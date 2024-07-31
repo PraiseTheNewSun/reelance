504558d4a7639d803b3b3f37fe441c541f523fa5
PraiseTheNewSun <142599118+PraiseTheNewSun@users.noreply.github.com>
2024-07-31 07:19:58 -0700

    Create build.sh
    wrote the build file

---
+++ build.sh
@@ -0,0 +1,6 @@
+pip3 install Django==5.0.3 pillow==10.2.0 pytz==2024.1 requests==2.31.0 sqlparse==0.4.4 tzdata==2024.1 urllib3==2.2.1 whitenoise==6.6.0
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic
+echo
+echo yes
